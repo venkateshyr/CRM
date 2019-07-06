8c5def7fa710c241f978aee9f8b7590bcd161790
Venkatesh YR <venkateshyr@hotmail.com>
2019-07-06 15:13:16 +0530

    Additional comments

--- src/com/xworks/WebCustomerTracker/dao/search/SearchDao.java
+++ src/com/xworks/WebCustomerTracker/dao/search/SearchDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.xworks.WebCustomerTracker.dto.CRMDto;
 
 //this is to check to comment on github
+//Gani comment
 public interface SearchDao {
 	public List search();
 	public String update();
